1c0b3744503ed2e8105fb45760be7cff7c595817
Eliott Legendre <eliott.legendre@imanage.com>
2021-04-30 15:01:03 +0200

    fix build

--- build.py
+++ build.py
@@ -7,7 +7,7 @@ use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
 use_plugin("python.install_dependencies")
-# use_plugin("pypi:pybuilder_docker", version="0.3.0")
+use_plugin("pypi:pybuilder_docker", version="0.3.0")
 
 name = "hello-flask"
 version = '0.0.dev1'
@@ -16,14 +16,15 @@ default_task = "publish"
 
 @init
 def set_properties(project):
-    project.build_depends_on("bump2version")
-    project.build_depends_on("flask==1.1.1")
-    project.build_depends_on("gunicorn==20.0.4")
+    project.depends_on("flask==1.1.1")
     project.depends_on('json-logging')
     project.depends_on('pyyaml')
     project.include_file("my_app.static.css", "style.css")
     project.include_file("my_app.templates", "index.html")
     # project.include_file("my_app.openai", "vectorizer.pkl")
+
+    project.build_depends_on("bump2version")
+    project.build_depends_on("gunicorn==20.0.4")
 
     project.set_property('coverage_break_build', False)
 
